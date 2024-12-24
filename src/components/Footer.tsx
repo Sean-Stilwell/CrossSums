ab519b59a81e1e69d54ebc90981f68b2c16670c0
Sean-Stilwell <sstil051@uottawa.ca>
2024-12-23 21:52:46 -0500

    Adding language toggle in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { AppBar, Toolbar, Typography, Container } from '@mui/material';
+import { AppBar, Toolbar, Typography, Container, Box, Button } from '@mui/material';
 import MuiLink from '@mui/material/Link';
 import { getDictionary } from '../i18n/dictionaries';
 
@@ -15,9 +15,16 @@ export default async function Footer(props: FooterProps) {
         <AppBar position="fixed" sx={{ top: 'auto', bottom: 0 }}>
             <Container maxWidth="md">
                 <Toolbar>
-                    <Typography variant="body1" color="inherit">
-                        {t.footer.message} <MuiLink  href="https://seanstilwell.ca/" color="inherit">Sean Stilwell</MuiLink>
-                    </Typography>
+                    <Box sx={{ flexGrow: 1 }}>
+                        <Typography variant="body1" color="inherit">
+                            {t.footer.message} <MuiLink  href="https://seanstilwell.ca/" color="inherit">Sean Stilwell</MuiLink>
+                        </Typography>
+                    </Box>
+                    <Box sx={{ flexGrow: 0 }}>
+                        <MuiLink href={lang === 'en' ? '/fr' : '/en'} color="inherit" underline="none">
+                            <Button color="inherit">{lang === 'en' ? 'FR' : 'EN'}</Button>
+                        </MuiLink>
+                    </Box>
                 </Toolbar>
             </Container>
         </AppBar>
